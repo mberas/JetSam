961cdb48ff981c4789a625d72fbec5e621c10e99
Manuel Beras <manuel.beras@gmail.com>
2023-02-23 19:18:10 -0500

    Configured eslint/prettier for code formating.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Fragment } from "react";
-import { Header, Footer, Contacts, Products, Testimonials } from "./components";
+import { Header, Footer, Contacts, Products, Testimonials } from './components';
 
 export default function App() {
-	return (
-		<Fragment>
-			<Header />
-			<Products />
-			<Testimonials />
-			<Contacts />
-			<Footer />
-		</Fragment>
-	);
+  return (
+    <>
+      <Header />
+      <Products />
+      <Testimonials />
+      <Contacts />
+      <Footer />
+    </>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path="/*" element={<App />} />
-			</Routes>
-		</Router>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
